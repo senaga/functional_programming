da92efcbc38600f1b02492d43962bcb11e204921
Marcelo Senaga <marcelo.senaga@tecnogroup.com.br>
2015-08-11 13:55:16 -0300

    teste

---
+++ src/main/scala-2.11/ch02/fibonacci/Fibonacci.scala
@@ -0,0 +1,22 @@
+package ch02.fibonacci
+
+object Fibonacci extends App {
+
+  def fib(n: Int): Int = {
+    if(n <= 1) n
+    else fib(n-1) + fib(n-2)
+  }
+
+  def fib2( n : Int) : Int = {
+    def loop(n: Int, a: Int, b: Int): Int = n match {
+      case 0 => a
+      case _ => loop(n-1, b, a+b)
+    }
+    loop( n, 0, 1)
+  }
+
+  println("Fib1")
+  (0 to 5).foreach(n => println(s"f($n) = " + fib(n)))
+  println("\nFib2")
+  (0 to 5).foreach(n => println(s"f($n) = " + fib2(n)))
+}
